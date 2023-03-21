10f54d290e6c47b83eb1ae2ff9c86d309b6244b8
David <parrydavid@live.co.uk>
2023-03-21 19:47:14 +0000

    added an alert function, that tells user about leaving blank inputs (but doesn't seem to be working as of yet), also added the handlesubmit function

--- src/pages/authentication/SignUpForm.js
+++ src/pages/authentication/SignUpForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import styles from '../../design/SignInUpForm.module.css';
 import btnStyles from '../../design/Button.module.css';
 import appStyles from '../../App.module.css';
 
-import { Form, Button, Image, Row, Container, Col } from 'react-bootstrap';
+import { Form, Button, Image, Row, Container, Col, Alert } from 'react-bootstrap';
+import axios from 'axios';
 
 const SignUpForm = () => {
   const [signUpData, setSignUpData] = useState({
@@ -15,31 +16,49 @@ const SignUpForm = () => {
   })
   const {username, password1, password2} = signUpData;
 
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
+
   const handleChange = (event) => {
     setSignUpData({
       ...signUpData,
       [event.target.name]: event.target.value,
     });
   } 
-  
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post('/dj-rest-auth/registration/', signUpData);
+      history.push('/signin');
+    } catch (err) {
+      setErrors(err.response?.data)
+    }
+  }
 
   return (
     <Row className={styles.Row}>
       <Col className='my-auto py-2 p-md-2' md={6}>
         <Container className={`${appStyles.Content} p-4`}>
           <h1 className={styles.Header}>Sign Up</h1>
-          <Form>
+
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className='d-none'>Username</Form.Label>
               <Form.Control
-              className={styles.Input}
-              type="text"
-              placeholder="Enter Username"
-              name="username"
-              value={username}
-              onChange={handleChange}
+                className={styles.Input}
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant='warning' key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password1">
               <Form.Label className='d-none'>Password</Form.Label>
