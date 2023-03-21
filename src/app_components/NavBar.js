a7793d38d8ce1f58c888b220ec435c8ccde7dc66
David <parrydavid@live.co.uk>
2023-03-21 14:59:29 +0000

    set up a basic navbar with links to signin/up, a home link and a logo on the left hand side. Also did some very basic modifications for where the text is on the screen

--- src/app_components/NavBar.js
+++ src/app_components/NavBar.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import Coffee_Bean_Logo from '../images/Coffee_Bean_Logo.png'
 
 const NavBar = () => {
   return (
-    <div>
-        Testing Navbar is connected
-    </div>
+    <Navbar expand="lg" fixed="top">
+        <Container>
+            <Navbar.Brand href="#home">
+                <img src={Coffee_Bean_Logo} alt="logo" height="45"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto text-right">
+                <Nav.Link href="#home">
+                    <i className="fas fa-home" />Home
+                    </Nav.Link>
+                <Nav.Link href="#link">
+                    <i className="fas fa-sign-in-alt"/>Sign In
+                    </Nav.Link>
+                <Nav.Link href="#link">
+                    <i className="fas fa-user-plus"/>Sign Up
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
   )
 };
 
