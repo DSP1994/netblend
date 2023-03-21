5a6a10c3b4fc6b107f7ba609ae5a38f4637b3703
David <parrydavid@live.co.uk>
2023-03-21 15:45:11 +0000

    added a few styles and hovers to my NavBar, colours taken from Coolors. Also changed navbar expand to 'sm' for easier viewing whilst coding

--- src/app_components/NavBar.js
+++ src/app_components/NavBar.js
@@ -5,23 +5,26 @@ import styles from '../design/NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <Navbar className={styles.NavBar} expand="lg" fixed="top">
+    <Navbar className={styles.NavBar} expand="sm" fixed="top">
         <Container>
-            <Navbar.Brand href="#home">
+            <Navbar.Brand>
                 <img src={Coffee_Bean_Logo} alt="logo" height="45"/>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-right">
-                <Nav.Link href="#home">
-                    <i className="fas fa-home" />Home
-                    </Nav.Link>
-                <Nav.Link href="#link">
-                    <i className="fas fa-sign-in-alt"/>Sign In
-                    </Nav.Link>
-                <Nav.Link href="#link">
-                    <i className="fas fa-user-plus"/>Sign Up
-                    </Nav.Link>
+                <Nav.Link >
+                    <i className="fas fa-home"></i>
+                    Home
+                </Nav.Link>
+                <Nav.Link>
+                    <i className="fas fa-sign-in-alt"></i>
+                    Sign In
+                </Nav.Link>
+                <Nav.Link>
+                    <i className="fas fa-user-plus"></i>
+                    Sign Up
+                </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
