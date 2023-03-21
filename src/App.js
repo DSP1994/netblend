27ad870018173bc5c627a5b51f1723ac4e71884a
David <parrydavid@live.co.uk>
2023-03-21 16:38:31 +0000

    I created a 'page not found' page incase a user accidentally inputs the incorrect url, and told them to direct themselves back to the main pages

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ function App() {
           <Route exact path='/' render={() => <h1>Home Page Tester</h1>} />
           <Route exact path='/signin' render={() => <h1>Sign In Tester</h1>} />
           <Route exact path='/signup' render={() => <h1>Sign Up Tester</h1>} />
+          <Route render={
+              ()=>
+              <p>
+                Coffee Beans reach doesn't extend to this page! Our apologies. Please click on one of the links in the Navigation Bar to bring you back to our delicious coffee pages!
+              </p>}
+              />
         </Switch>
       </Container>
     </div>
