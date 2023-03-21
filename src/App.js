1c5d00491d93153e4e7ccf150b8bd7cd0a390da0
David <parrydavid@live.co.uk>
2023-03-21 16:00:07 +0000

    created the base line of some new pages to get me started on how I want to lay out the website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import styles from './App.module.css';
 import NavBar from './app_components/NavBar';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.TitlePages}>
+        <h1>Home Page Tester</h1>
+        <h1>Sign In Tester</h1>
+        <h1>Sign Up Tester</h1>
+      </Container>
     </div>
   );
 }
