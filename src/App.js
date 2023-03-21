5104690f318ba9576b3768d83161c7d631f7f244
David <parrydavid@live.co.uk>
2023-03-21 16:04:47 +0000

    installed react router, and added in the switch function in the the App.js, put place holders for a few pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import styles from './App.module.css';
 import NavBar from './app_components/NavBar';
-import { Container } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container'
+import {Route, Switch} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.TitlePages}>
-        <h1>Home Page Tester</h1>
-        <h1>Sign In Tester</h1>
-        <h1>Sign Up Tester</h1>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Home Page Tester</h1>} />
+          <Route exact path='/' render={() => <h1>Sign In Tester</h1>} />
+          <Route exact path='/' render={() => <h1>Sign Up Tester</h1>} />
+        </Switch>
       </Container>
     </div>
   );
