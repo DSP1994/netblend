3cd004a272241450dcaaba86bde6ea6b3e94e687
David <parrydavid@live.co.uk>
2023-03-22 12:47:05 +0000

    imported the styles I will need from the design folder

--- src/pages/SignInForm.js
+++ src/pages/SignInForm.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import styles from "../design/SignInUpForm.module.css";
+import btnStyles from "../design/Button.module.css";
+import appStyles from "../App.module.css"
+
 const SignInForm = () => {
   return (
     <div>SignInForm</div>
