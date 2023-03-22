487d2c558cbcb1efd9e49b45bff12d9f2ee0aa8f
David <parrydavid@live.co.uk>
2023-03-22 15:35:10 +0000

    these interceptors have been added into the CurrentUserContext, and will keep a user logged in for 24 hours minimum (unless they decide to stay logged in). This is the default time length for a refresh token using axios interceptors

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,5 +1,8 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
+import { axiosRes } from '../netblend_api/axiosDefaults';
+import { response } from 'msw';
+import { useHistory } from 'react-router-dom';
 
 export const CurrentUserContext = createContext()
 export const SetCurrentUserContext = createContext()
@@ -9,10 +12,11 @@ export const useSetCurrentUser = () => useContext(SetCurrentUserContext)
 
 export const CurrentUserProvider = ({children}) => {
   const [currentUser, setCurrentUser] = useState(null)
+  const history = useHistory()
 
   const handleMount = async () => {
     try {
-      const {data} = await axios.get('dj-rest-auth/user/')
+      const {data} = await axiosRes.get('dj-rest-auth/user/')
       setCurrentUser(data) 
     } catch (error) {}
   }
@@ -20,6 +24,28 @@ export const CurrentUserProvider = ({children}) => {
   useEffect(() => {
     handleMount()
   }, []);
+
+  useMemo(() => {
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (error.response?.status === 401){
+          try {
+            await axios.post('/dj-rest-auth/token/refresh/')
+          } catch (error) {
+            setCurrentUser(prevCurrentUser => {
+              if (prevCurrentUser){
+                history.push("/signin")
+              }
+              return null
+            })
+          }
+          return axios(error.config)
+        }
+        return Promise.reject(error)
+      }
+    )
+  })
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
