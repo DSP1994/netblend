4317f7cd761330d0f5499bce08c9acf93af82173
David <parrydavid@live.co.uk>
2023-03-22 17:25:53 +0000

    added the standard styles I am going to need for this page

--- src/pages/uploads/UploadForm.js
+++ src/pages/uploads/UploadForm.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import upload_image from '../../design/upload_image.jpg'
 
+import btnStyles from '../../design/Button.module.css'
+import appStyles from '../../App.module.css'
+
 const UploadForm = () => {
   return (
     <div>UploadForm</div>
