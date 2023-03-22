45990a4c5a514dae79a9612b4df763a5c4550ed4
David <parrydavid@live.co.uk>
2023-03-22 15:12:03 +0000

    in my signin form, I have called my set currentuser and removed imports that are no longer applicable to the form

--- src/pages/authentication/SignInForm.js
+++ src/pages/authentication/SignInForm.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
 import styles from "../../design/SignInUpForm.module.css";
@@ -7,10 +7,10 @@ import appStyles from "../../App.module.css"
 
 import { Row, Col, Container, Image, Form, Button, Alert } from 'react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
-import { SetCurrentUserContext } from '../../App';
+import { useSetCurrentUser } from '../../contexts/CurrentUserContext';
 
 function SignInForm () {
-    const setCurrentUser = useContext(SetCurrentUserContext);
+    const setCurrentUser = useSetCurrentUser();
 
     const [signInData, setSignInData] = useState({
         username: "",
