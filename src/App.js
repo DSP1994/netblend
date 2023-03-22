64994f4ab35bb1513f2f80f83f2f3480953e8f5c
David <parrydavid@live.co.uk>
2023-03-22 15:04:45 +0000

    For best practice, I have moved my CurrentUserContext into its own folder, to ensure that the code stays clean and readable. This is something I picked up from the CI tutorial, and it looks a lot more professional. This includes moving most of the code I just made from app.js into CurrentUserContext.js

--- src/App.js
+++ src/App.js
@@ -5,30 +5,9 @@ import {Route, Switch} from 'react-router-dom'
 import './netblend_api/axiosDefaults'
 import SignUpForm from './pages/authentication/SignUpForm';
 import SignInForm from './pages/authentication/SignInForm';
-import { createContext, useEffect, useState } from 'react';
-import axios from 'axios';
-
-export const CurrentUserContext = createContext()
-export const SetCurrentUserContext = createContext()
-
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null)
-
-  const handleMount = async () => {
-    try {
-      const {data} = await axios.get('dj-rest-auth/user/')
-      setCurrentUser(data) 
-    } catch (error) {}
-  }
-
-  useEffect(() => {
-    handleMount()
-  }, [])
-
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
         <div className={styles.App}>
           <NavBar />
           <Container className={styles.TitlePages}>
@@ -45,8 +24,6 @@ function App() {
             </Switch>
           </Container>
         </div>
-      </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
   );
 }
 
