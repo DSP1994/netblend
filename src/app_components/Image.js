1c3e1785e05517cb5faf5dbcae3b910a977e1dbe
David <parrydavid@live.co.uk>
2023-03-22 18:10:14 +0000

    added in the return code to produce a spinner on the page, however I still need to create some styles for it, otherwise it will not show on my page

--- src/app_components/Image.js
+++ src/app_components/Image.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Spinner } from 'react-bootstrap'
 
 const Image = () => {
+    const { spinner, src, message } = props
   return (
-    <div>Image</div>
+    <div className={`${styles.Image} p-4`}>
+        {spinner && <Spinner animation='border' />}
+        {src && <img src={src} alt={message} />}
+        {message && <p className='mt-4'>{message}</p>}
+    </div>
   )
 }
 
-export default Image
+export default Image;
