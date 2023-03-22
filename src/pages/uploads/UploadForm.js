d23bc935e44df7789142122587c285c5ad6a9b7d
David <parrydavid@live.co.uk>
2023-03-22 17:24:36 +0000

    created uploadform and amended upload_image name due to naming conventions

---
+++ src/pages/uploads/UploadForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import upload_image from '../../design/upload_image.jpg'
+
+const UploadForm = () => {
+  return (
+    <div>UploadForm</div>
+  )
+}
+
+export default UploadForm
