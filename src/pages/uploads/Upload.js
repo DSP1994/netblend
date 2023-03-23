c09ba6f4e3f77971d169b3f11f44ed3289a69d7c
David <parrydavid@live.co.uk>
2023-03-23 20:48:41 +0000

    created a like ternary to show the number of likes, and whether a user can like it based on their logged in or out status

--- src/pages/uploads/Upload.js
+++ src/pages/uploads/Upload.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from '../../design/Upload.module.css'
 import { useCurrentUser } from '../../contexts/CurrentUserContext'
-import { Card, Media } from 'react-bootstrap'
+import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Avatar from '../../app_components/Avatar'
 
@@ -34,7 +34,31 @@ const Upload = (props) => {
     </Link>
     <Card.Body>
         {title && <Card.Title className='text-center'>{title}</Card.Title>}
-        {content && <Card.Text>{context}</Card.Text>}
+        {content && <Card.Text>{content}</Card.Text>}
+        <div className={styles.UploadBar}>
+            {is_owner ? (
+                <OverlayTrigger placement='top' overlay={<Tooltip>Vain! Get your friends to like it!</Tooltip>}>
+                    <i className='far fa-heart' />
+                </OverlayTrigger>
+            ) : like_id ? (
+                <span onClick={()=>{}}>
+                    <i className={`fas fa-heart ${styles.Heart}`} />
+                </span>
+            ) : currentUser ? (
+                <span onClick={()=>{}}>
+                    <i className={`far fa-heart ${styles.HeartOutline}`} />
+                </span>
+            ) : (
+                <OverlayTrigger placement='top' overlay={<Tooltip>Tasty huh..! Log in to like it!</Tooltip>}>
+                    <I className='far fa-heart' />
+                </OverlayTrigger>
+            )}
+            {likes_count}
+            <Link to={`/posts/${id}`}>
+                <i className='far fa-comments' />
+            </Link>
+            {comments_count}
+        </div>
     </Card.Body>
   </Card>
 }
