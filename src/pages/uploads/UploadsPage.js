912f17313ba8b2d3fd900c4bd75e074634de04c3
David <parrydavid@live.co.uk>
2023-03-23 21:18:30 +0000

    set up basic lay out for my uploadspage

---
+++ src/pages/uploads/UploadsPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Col, Row, Container, Form } from 'react-bootstrap'
+
+const UploadsPage = () => {
+
+
+  return (
+    <Row className='h-100'>
+        <Col className='py-2 p-0 p-lg-2' lg={8}>
+            <p>profs for mob</p>
+            <p>list of uploads</p>
+        </Col>
+        <Col md={4} className='d-none d-lg-block p-0 p-lg-2'>
+            <p>profs for pc</p>
+        </Col>        
+    </Row>
+  )
+}
+
+export default UploadsPage
