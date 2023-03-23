d5dc7dd9bd3bc5cae1b0298052e0ad578935e845
David <parrydavid@live.co.uk>
2023-03-23 21:47:26 +0000

    created a 'no results' image if the user has not followed/liked a post, and created a flow of images on the home page

--- src/pages/uploads/UploadsPage.js
+++ src/pages/uploads/UploadsPage.js
@@ -4,6 +4,11 @@ import { useLocation } from 'react-router';
 
 import appStyles from '../../App.module.css'
 import { axiosReq } from '../../netblend_api/axiosDefaults';
+import Upload from './Upload';
+
+import NoResults from '../../images/no-results.jpg'
+import ImageSpinner from '../../app_components/ImageSpinner';
+
 
 function UploadsPage({ message, filter = ''}) {
     const [posts, setPosts] = useState({results: []});
@@ -29,7 +34,21 @@ function UploadsPage({ message, filter = ''}) {
     <Row className='h-100'>
         <Col className='py-2 p-0 p-lg-2' lg={8}>
             <p>profs for mob</p>
-            <p>list of uploads</p>
+            {hasLoaded ? (
+                <>
+                    {posts.results.length
+                        ? posts.results.map((post) => (
+                            <Upload key={post.id} {...post} setPosts={setPosts} />
+                        ))
+                     : <Container className={appStyles.Content}>
+                        <ImageSpinner src={NoResults} message={message}/>
+                       </Container>}
+                </>
+            ) : (
+                <Container className={appStyles.Content}>
+                    <ImageSpinner spinner />
+                </Container>
+            )}
         </Col>
         <Col md={4} className='d-none d-lg-block p-0 p-lg-2'>
             <p>profs for pc</p>
