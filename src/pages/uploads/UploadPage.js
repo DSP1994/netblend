9b07b774e8b0834d8ba839a772e1e710763513fb
David <parrydavid@live.co.uk>
2023-03-23 20:08:33 +0000

    set up my handlemount layout

--- src/pages/uploads/UploadPage.js
+++ src/pages/uploads/UploadPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import {Row, Col, Container} from "react-bootstrap"
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
@@ -8,6 +8,16 @@ import appStyles from '../../App.module.css'
 const UploadPage = () => {
     const {id} = useParams();
     const [upload, setUpload] = useState({ results: []})
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    })
   return (
     <Row className='h-100'>
         <p>mobile</p>
