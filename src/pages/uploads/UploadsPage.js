953a2a1455a73d6f3510437467545b5df736d080
David <parrydavid@live.co.uk>
2023-03-23 21:38:08 +0000

    created a const to fetch posts for user to view, created the beginnings of a 'spinner' to give the user an idea that something is happening

--- src/pages/uploads/UploadsPage.js
+++ src/pages/uploads/UploadsPage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Row, Container, Form } from 'react-bootstrap'
+import { useLocation } from 'react-router';
 
 import appStyles from '../../App.module.css'
+import { axiosReq } from '../../netblend_api/axiosDefaults';
 
-function UploadsPage() {
+function UploadsPage({ message, filter = ''}) {
+    const [posts, setPosts] = useState({results: []});
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const {pathname} = useLocation();
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const {data} = await axiosReq.get(`/posts/?${filter}`)
+                setPosts(data)
+                setHasLoaded(true)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        setHasLoaded(false)
+        fetchPosts()
+    }, [filter, pathname])
+
   return (
     <Row className='h-100'>
         <Col className='py-2 p-0 p-lg-2' lg={8}>
