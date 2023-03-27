9db59106aba0c503f9f5ed5e538387b4539b1fe9
David <parrydavid@live.co.uk>
2023-03-27 14:07:43 +0000

    created comment form, and where the user has a profile id

--- src/pages/uploads/UploadPage.js
+++ src/pages/uploads/UploadPage.js
@@ -12,7 +12,7 @@ import { useCurrentUser } from '../../contexts/CurrentUserContext'
 function UploadPage() {
     const {id} = useParams();
     const [post, setPost] = useState({ results: []})
-    const useCurrentUser = useCurrentUser();
+    const currentUser = useCurrentUser();
     const profile_image = currentUser?.profile_image;
     const [comments, setComments] = useState({results : [] });
 
@@ -37,7 +37,17 @@ function UploadPage() {
         <Upload {...post.results[0]} setPost={setPost} postPage/>
         <Col className='py-2 p-0 p-lg-2' lg={8}>
             <Container className={appStyles.Content}>
-                Comment
+                {currentUser ? (
+                    <CommentForm
+                    profile_id={currentUser.profile_id}
+                    profileImage={profile_image}
+                    post={id}
+                    setPost={setPost}
+                    setComments={setComments}
+                    />
+                    ) : comments.results.length ? (
+                    "Comments"
+                    ) : null}              
             </Container>
         </Col>
         <Col lg={4} className='d-none d-lg-block p-0 p-lg-2'>
