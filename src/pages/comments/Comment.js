6945ae574403808190b28b9eb3893986f5a4d9dd
David <parrydavid@live.co.uk>
2023-03-27 14:35:23 +0000

    created comment const, and added media, links and the avatar

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Media } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Avatar from '../../app_components/Avatar'
 import styles from '../../design/Comment.module.css'
 
 const Comment = (props) => {
+    const {profile_id, profile_image, owner, updated_at, content} = props;
   return (
-    <div></div>
+    <div>
+        <Media>
+            <Link to={`/profiles/${profile_id}`}>
+                <Avatar src={profile_image} />
+            </Link>
+            <Media.Body className='align-self-center ml-2'>
+                <span className={styles.Owner}>{owner}</span>
+                <span className={styles.Date}>{updated_at}</span>
+                <p>{content}</p>
+            </Media.Body>
+        </Media>
+    </div>
   )
 }
 
