c8a83ba2ebc75145667edcd4c4478a5bda1fcd07
David <parrydavid@live.co.uk>
2023-03-28 14:13:33 +0000

    created const's and useeffect for when a profile has loaded, and set a basic row/col for profile lay out

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -9,6 +9,30 @@ import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
 
 function ProfilePage() {
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        setHasLoaded(true);
+    }, [])
+
+    const mainProfile =(
+        <>
+            <Row noGutters className='px-3 text-center'>
+                <Col lg={3} className='text-lg-left'>
+                    <p>image goes here</p>
+                </Col>
+                <Col lg={6}>
+                    <h3 className='m-2'>Prof User</h3>
+                    <p>stats</p>
+                </Col>
+                <Col lg={3} className='text-lg-right'>
+                    <p>follow b</p>
+                </Col>
+                <Col className='p-3'>Prof Cont</Col>
+            </Row>
+        </>
+    )
   return (
     <div>ProfilePage</div>
   )
