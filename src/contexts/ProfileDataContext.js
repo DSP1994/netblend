1c76a54113073292aec546ed5752fa626276abdb
David <parrydavid@live.co.uk>
2023-03-28 13:48:35 +0000

    created profiledatacontext js file, and created const needed (following current user context as a base line

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,11 @@
+import { createContext, useContext } from "react";
+
+export const ProfileDataContext = createContext();
+export const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+    
+}
