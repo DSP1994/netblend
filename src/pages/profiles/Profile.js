c37f9d576d503b360a95c8a9675d7e68c3ab6bf0
David <parrydavid@live.co.uk>
2023-03-28 13:35:36 +0000

    created avatar and Link (auto imported) in divs to show user prof pic and name

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from '../../design/Profile.module.css'
 import btnStyle from '../../design/Button.module.css'
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { Link } from 'react-router-dom';
+import Avatar from '../../app_components/Avatar';
 
 const Profile = (props) => {
     const {profile, mobile, imageSize=55} = props;
@@ -9,11 +11,19 @@ const Profile = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
-  return (
-    <div>
-
+  return <div
+    className={`my-3 d-flex align-items-center ${mobile && 'flex-colum'}`}
+    >
+        <div>
+            <Link className='align-self-center' to={`/profiles/${id}/}`}>
+                <Avatar src={image} height={imageSize} />
+            </Link>
+        </div>
+        <div className={`mx-2 ${styles.WordBreak}`}>
+            <strong>{owner}</strong>
+        </div>
     </div>
-  )
+
 }
 
 export default Profile
