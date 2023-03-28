305f0a5d07dd4cb0fdd1d76b45cea287d18e72a8
David <parrydavid@live.co.uk>
2023-03-28 13:03:09 +0000

    created a handleMount const for getting the follower count for most popular users

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
+import { axiosReq } from '../../netblend_api/axiosDefaults'
 import appStyles from '../../App.module.css'
 
 const PopularProfiles = () => {
+  const [profileData, setProfileData] = useState({
+    // pageProfile: { results; []},
+    popularProfiles:  {results: []},
+  });
+
+  const {popularProfiles} = profileData;
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const{data} = await axiosReq.get(
+          '/profiles/?ordering=-follwers_count'
+        );
+        setProfileData(prevState => ({
+          ...prevState,
+          popularProfiles: data,
+        }))
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    
+    handleMount()
+  })
+
   return (
     <Container className={appStyles.Content}>
         <p>Pop Profs</p>
