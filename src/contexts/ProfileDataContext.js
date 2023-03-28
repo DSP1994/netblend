8c313a04e9af11a02a53b4bc5440b66f598825f0
David <parrydavid@live.co.uk>
2023-03-28 13:50:08 +0000

    added into const PDP, copied from popular profiles

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -7,5 +7,28 @@ export const useProfileData = () => useContext(ProfileDataContext);
 export const useSetProfileData = () => useContext(SetProfileDataContext);
 
 export const ProfileDataProvider = ({ children }) => {
-    
-}
+    const [profileData, setProfileData] = useState({
+    // pageProfile: { results; []},
+    popularProfiles:  {results: []},
+    });
+
+    const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        const handleMount = async () => {
+        try {
+            const{data} = await axiosReq.get(
+            '/profiles/?ordering=-followers_count'
+            );
+            setProfileData(prevState => ({
+            ...prevState,
+            popularProfiles: data,
+            }))
+        } catch (error) {
+            console.log(error)
+        }
+        };
+        
+        handleMount()
+    }, [currentUser])
+    }
