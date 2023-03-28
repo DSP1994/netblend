ee56205c73d3c5ed29dcd3f9d013fda0fb7b1d82
David <parrydavid@live.co.uk>
2023-03-28 17:55:00 +0000

    created useRedirect.js file in the hooks folder, and started to creat a const that will redirect a user from certain pages

---
+++ src/hooks/useRedirect.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router"
+
+export const useRedirect = (userAuthStatus) => {
+    const history = useHistory();
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                
+            } catch (error) {
+                
+            }
+        }
+    })
+}
