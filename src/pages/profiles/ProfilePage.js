a21470373fedbe7d0f69e2fe59f5c2a160e93e22
David <parrydavid@live.co.uk>
2023-03-28 14:09:01 +0000

    added imports

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import { Row, Container, Col } from 'react-bootstrap';
+import ImageSpinner from '../../app_components/ImageSpinner';
+import styles from '../../design/ProfilePage.module.css';
+import appStyles from '../../App.module.css'
+import btnStyles from '../..styles/Button.module.css'
+import PopularProfiles from './PopularProfiles';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+
 
 function ProfilePage() {
   return (
