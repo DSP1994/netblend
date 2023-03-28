92560c73c252adc2fa91841961f5c2f6fb109dd7
David <parrydavid@live.co.uk>
2023-03-28 13:30:09 +0000

    created all the const I will need for my profile.js

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import styles from '../../design/Profile.module.css'
 import btnStyle from '../../design/Button.module.css'
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
-const Profile = () => {
+const Profile = (props) => {
+    const {profile, mobile, imageSize=55} = props;
+    const {id, follwing_id, image, owner} = profile;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
   return (
     <div>
 
