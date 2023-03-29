397f494955f2a5af6660f5855e7ee46d33f59cab
David <parrydavid@live.co.uk>
2023-03-29 18:10:25 +0000

    added redirect

--- src/pages/articles/CreateArticleForm.js
+++ src/pages/articles/CreateArticleForm.js
@@ -7,8 +7,10 @@ import btnStyles from '../../design/Button.module.css'
 import styles from '../../design/CreateArticleForm.module.css'
 
 import { axiosReq } from '../../netblend_api/axiosDefaults'
+import {useRedirect} from '../../hooks/useRedirect';
 
 function CreateArticleForm() {
+    useRedirect('loggedOut');
     const [errors, setErrors] = useState();
     const history = useHistory();
     const [articleData, setArticleData] = useState({
@@ -16,14 +18,12 @@ function CreateArticleForm() {
         content: '',
     })
 
-    const {
-        title, content,
-    } = articleData;
+    const {title, content} = articleData;
 
     const handleChange = (event) => {
         setArticleData({
-        ...articleData,
-        [event.target.name]: event.target.value,
+            ...articleData,
+            [event.target.name]: event.target.value,
         });
     };
 
@@ -37,7 +37,9 @@ function CreateArticleForm() {
         try {
             const {data} = await axiosReq.post('/articles/', formData);
             history.push(`/articles/${data.id}`);
+            console.log(formData)
         } catch (error) {
+            console.log(error)
             if (error.response?.status !== 401){
                 setErrors(error.response?.data);
             }
@@ -52,11 +54,11 @@ function CreateArticleForm() {
             <Form.Group>
                 <Form.Label>Caption</Form.Label>
                 <Form.Control 
-                type='text'
-                name='title'
-                value={title}
-                onChange={() => handleChange}
-                aria-label='title'
+                    type='text'
+                    name='title'
+                    value={title}
+                    onChange={handleChange}
+                    aria-label='title'
                 />
             </Form.Group>
             {errors?.title?.map((message, idx) => (
@@ -67,12 +69,12 @@ function CreateArticleForm() {
             <Form.Group>
                 <Form.Label>Description</Form.Label>
                 <Form.Control 
-                as='textarea'
-                rows={8}
-                name='content'
-                value={content}
-                onChange={() => handleChange}
-                aria-label='content'
+                    as='textarea'
+                    rows={8}
+                    name='content'
+                    value={content}
+                    onChange={handleChange}
+                    aria-label='content'
                 />
             </Form.Group>
             {errors?.content?.map((message, idx) => (
