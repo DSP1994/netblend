58400da5937554b639eeca015a4a4297f58b397b
David <parrydavid@live.co.uk>
2023-03-29 18:49:27 +0000

    imported react items and user created items needed for Articles Page

--- src/pages/articles/ArticlesPage.js
+++ src/pages/articles/ArticlesPage.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react'
 import { Container, Form } from 'react-bootstrap'
 import { useLocation } from 'react-router-dom';
 
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { axiosReq } from '../../netblend_api/axiosDefaults';
+import ImageSpinner from '../../app_components/ImageSpinner';
+import Article from './Article';
+
+
 function ArticlePage({message, filter = ''}) {
     const [article, setArticle] = useState({results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
