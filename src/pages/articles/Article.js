af2bb7bc0c3a116181a8d10fe887a3c0fa38c2e7
David <parrydavid@live.co.uk>
2023-03-29 18:14:23 +0000

    imported currentuser and history from react

--- src/pages/articles/Article.js
+++ src/pages/articles/Article.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
 function Article(props) {
     const [
         id, owner, is_owner, title, content, created_on,
         modified_on, profile_id, profile_image,
-    ] = props
+    ] = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+    const history = useHistory();
+
   return (
     <div>Article</div>
   )
