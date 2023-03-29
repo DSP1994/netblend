15324a500c9d1fb2a238ec5fe180981baa0003da
David <parrydavid@live.co.uk>
2023-03-29 20:27:38 +0000

    created my handleMount

--- src/pages/articles/ArticlePage.js
+++ src/pages/articles/ArticlePage.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {Row, Col, Container} from "react-bootstrap";
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { axiosReq } from '../../netblend_api/axiosDefaults';
 import Article from './Article';
 
 function ArticlePage() {
     const {id} = useParams();
     const [article, setArticle] = useState({results: []});
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{data: article}] = await Promise.all([
+                    axiosReq.get(`/article${id}`),
+                ]);
+                setArticle({results: [article]})
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        handleMount();
+    }, [id])
 
   return (
     <Row>
