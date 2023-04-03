afe2934c64ec178c516800cb64c22b17f6a1f3eb
David <parrydavid@live.co.uk>
2023-04-03 15:56:44 +0000

    started eventPage lay out with basic imports

---
+++ src/pages/events/EventPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Event from './Event';
+
+function EventPage() {
+  return (
+    <Container>
+        <Event />
+    </Container>
+  )
+}
+
+export default EventPage
