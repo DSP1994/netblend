666cc91411826f8b5faf8e4b1b7b5f3014e79af0
David <parrydavid@live.co.uk>
2023-04-03 16:30:06 +0000

    created the hasLoaded function that will show a loading spinner if more events need to be loaded into the page, and will scroll continously onto the next page

--- src/pages/events/EventsPage.js
+++ src/pages/events/EventsPage.js
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { Container, Form } from 'react-bootstrap'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useLocation } from 'react-router-dom';
+import ImageSpinner from '../../app_components/ImageSpinner';
 import { axiosReq } from '../../netblend_api/axiosDefaults';
+import { fetchMoreData } from '../../utils/utils';
+import Event from './Event';
 
 function EventsPage({message, filter = ''}) {
     const [events, setEvents] = useState({results: []});
@@ -45,11 +48,31 @@ function EventsPage({message, filter = ''}) {
                 />
             </Form>
         </div>
-        <p>Loaded, or no?
-            <InfiniteScroll />
-        </p>
+        {hasLoaded ? (
+            <>
+                {events.results.length ? (
+                    <InfiniteScroll 
+                        children={events.results.map((event) => (
+                            <Event key={event.id} {...event} setEvents={setEvents} />
+                        ))}
+                        dataLength={events.results.length}
+                        loader={<ImageSpinner spinner />}
+                        hasMore={!!events.next}
+                        next={() => fetchMoreData(events, setEvents)}
+                    />
+                ) : (
+                    <Container>
+                        <ImageSpinner message={message} />
+                    </Container>
+                )}
+            </>
+        ) : (
+            <Container>
+                <ImageSpinner spinner />
+            </Container>
+        )}
     </Container>
-  )
+  );
 }
 
 export default EventsPage
