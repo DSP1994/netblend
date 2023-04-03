c99a758dc79f0f961f0f1196209955c7a40538fd
David <parrydavid@live.co.uk>
2023-04-03 15:59:54 +0000

    created handleMount via the useEffect function

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import { useParams } from 'react-router-dom';
+import { axiosReq } from '../../netblend_api/axiosDefaults';
 import Event from './Event';
 
 function EventPage() {
     const {id} = useParams();
     const [event, setEvent] = useState({results: []});
 
+    useEffect(()=> {
+        const handleMount = async () => {
+            try {
+                const [{data:event}] = await Promise.all([
+                    axiosReq.get(`/events/${id}`),
+                ]);
+                setEvent({results: [event]});
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        handleMount();
+    }, [id]);
+    
   return (
     <Container>
         <Event />
