9feba0b13ce9b8ec046c3b0975b873406705466d
David <parrydavid@live.co.uk>
2023-04-03 15:58:07 +0000

    created const's needed for eventpage function

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
+import { useParams } from 'react-router-dom';
 import Event from './Event';
 
 function EventPage() {
+    const {id} = useParams();
+    const [event, setEvent] = useState({results: []});
+
   return (
     <Container>
         <Event />
