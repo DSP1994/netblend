1c0873c68244d2cf2b26d5ff0f4e54f3dcfbfe3e
David <parrydavid@live.co.uk>
2023-04-03 16:15:51 +0000

    created const's and imported from react router dom, and passed props into the function!

--- src/pages/events/EventsPage.js
+++ src/pages/events/EventsPage.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Form } from 'react-bootstrap'
 import InfiniteScroll from 'react-infinite-scroll-component'
+import { useLocation } from 'react-router-dom';
 
-function EventsPage() {
+function EventsPage({message, filter = ''}) {
+    const [events, setEvents] = useState({results: []});
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const {pathname} = useLocation();
+    const [query, setQuery] = useState('');
+
   return (
     <Container>
         <div>
