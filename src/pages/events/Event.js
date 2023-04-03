e17583c14ef5583239312e25b0e8ef5c780347c6
David <parrydavid@live.co.uk>
2023-04-03 14:54:54 +0000

    created handleDelete const, currently also goes nowhere, nor is it linked up

--- src/pages/events/Event.js
+++ src/pages/events/Event.js
@@ -5,6 +5,7 @@ import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
 import Avatar from '../../app_components/Avatar';
 import OwnerDropdown from '../../app_components/OwnerDropdown';
+import { axiosRes } from '../../netblend_api/axiosDefaults';
 
 function Event(props) {
 const {id, owner, profile_id, profile_image, created_on,
@@ -17,6 +18,16 @@ const history = useHistory();
 
 const handleEdit = () => {
     history.push(`/events/${id}/edit`);
+};
+
+const handleDelete = async () => {
+    try {
+        await axiosRes.delete(`/events/${id}/`);
+        history.goBack();
+    } catch (error) {
+        console.log(error)
+        
+    }
 };
 
   return (
