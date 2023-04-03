b9edcdeff9e10c04b9a28766a577a09b729eab90
David <parrydavid@live.co.uk>
2023-04-03 16:19:30 +0000

    created useEffect for a search function of the events pag

--- src/pages/events/EventsPage.js
+++ src/pages/events/EventsPage.js
@@ -1,13 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Form } from 'react-bootstrap'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useLocation } from 'react-router-dom';
+import { axiosReq } from '../../netblend_api/axiosDefaults';
 
 function EventsPage({message, filter = ''}) {
     const [events, setEvents] = useState({results: []});
     const [hasLoaded, setHasLoaded] = useState(false);
     const {pathname} = useLocation();
     const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        const fetchEvents = aysnc () => {
+            try {
+                const {data} = await axiosReq.get(`/events/?${filter}search=${query}`);
+                setEvents(data);
+                setHasLoaded(true)
+            } catch (error) {
+                console.log(error)
+            }
+        };
+
+        setHasLoaded(false);
+
+        const timer = setTimeout(() => {
+            fetchEvents();
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [filter, query, pathname])
 
   return (
     <Container>
