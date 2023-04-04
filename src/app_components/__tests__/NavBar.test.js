806d36a2eacb16465024da92b100815649e9eacd
David <parrydavid@live.co.uk>
2023-04-04 16:48:17 +0000

    created log in and out tests

--- src/app_components/__tests__/NavBar.test.js
+++ src/app_components/__tests__/NavBar.test.js
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 import { BrowserRouter as Router} from "react-router-dom"
 import NavBar from "../NavBar"
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 test('NavBar renders', () => {
   render(
@@ -10,6 +11,25 @@ test('NavBar renders', () => {
   );
 
 //   screen.debug();
-  const signInLink = screen.getByRole("link", { name: "Sign in" });
+  const signInLink = screen.getByRole("Link", { name: "Sign in" });
   expect(signInLink).toBeInTheDocument();
+});
+
+test('renders Sign in and Sign up buttons again on log out', async () => {
+  render(
+    <Router>
+      <CurrentUserProvider>
+        <NavBar />
+      </CurrentUserProvider>
+    </Router>,
+  );
+
+  const signOutLink = await screen.findByRole('link', { name: 'Sign Out' });
+  fireEvent.click(signOutLink);
+
+  const signInLink = await screen.findByRole('link', { name: 'Sign In' });
+  const signUpLink = await screen.findByRole('link', { name: 'Sign up' });
+
+  expect(signInLink).toBeInTheDocument();
+  expect(signUpLink).toBeInTheDocument();
 });
