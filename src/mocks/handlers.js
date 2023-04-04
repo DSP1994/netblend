07b4c2472a401f811845385a59f649a70252f0a4
David <parrydavid@live.co.uk>
2023-04-04 16:29:42 +0000

    set up handlers, and setupTests.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,3 +1,22 @@
+import {rest} from 'msw'
+
 const baseURL = 'https://netblend-api.herokuapp.com/'
 
-export const handlers = []
+/* Mock user */
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(ctx.json({
+            pk: 27,
+            username: "testing1",
+            email: "",
+            first_name: "",
+            last_name: "",
+            profile_id: 26,
+            profile_image: "https://res.cloudinary.com/dsp1994/image/upload/v1/media/../brown-profile-image_xcia4p"
+            })
+        )
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req,res,ctx) => {
+        return res(ctx.status(200))
+    })
+]
